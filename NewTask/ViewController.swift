8cba1fdbec5e10857297d5513cad520ee94e8eb1
Lado <Tsivtsivadzelado@yahoo.com>
2020-12-06 11:15:59 -0800

    first

---
+++ NewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTask
+//
+//  Created by Ladolado3911 on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
